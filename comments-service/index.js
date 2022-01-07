16bd3992c21d266d71f2ca0783baa7343b2cd6cd
iamprincesly <iamprincesly@gmail.com>
2022-01-07 23:51:54 +0100

    Implement post endpoint in comments service

--- comments-service/index.js
+++ comments-service/index.js
@@ -5,9 +5,23 @@ const { randomBytes } = require('crypto');
 const app = express();
 app.use(bodyParser.json());
 
+const commentsByPostId = {};
+
 app.get('/posts/:id/comments', (req, res) => {});
 
-app.post('/posts/:id/comments', (req, res) => {});
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+
+    const { content } = req.body;
+
+    const comments = commentsByPostId[req.params.id] || [];
+
+    comments.push({ id: commentId, content: content });
+
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(comments);
+});
 
 app.listen(4001, () => {
     console.log('Listening on port 4001');
